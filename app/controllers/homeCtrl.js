4615802088437680d4b23671c51f9cd4cf61e77d
Sean Wright <s.galen.wright@gmail.com>
2014-04-09 00:45:28 -0400

    Updated homeCtrl.js and index.html
    Added new methods to homeCtrl.js to allow the addition and deletion of
    items from the $scope.items object array
    Added data-ng-repeat and a form to display and submit new items

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -3,6 +3,36 @@
 */
 
 demoAngular.controller('HomeCtrl', function ($scope) {
-    $scope.CurrentPageName = "Home";
-    $scope.Header = "Hello";
+
+    init();
+
+    // init
+    //      initializes all the values attached to $scope
+    function init() {
+        $scope.CurrentPageName = "Home";
+        $scope.Header = "Hello";
+        $scope.items = [];
+    }
+
+    // addItem
+    //      creates a new item with the name provided by the $scope.newItem value
+    //      and then adds it to the object array $scope.items
+    $scope.addItem = function () {
+        var newItem = { "itemName": "", "id": "" };
+        newItem.itemName = $scope.newItem;
+
+        // This creates a GUID
+        newItem.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16)
+        });
+
+        $scope.items.push(newItem);
+    };
+
+    // removeItem
+    //      removes an item from the object array $scope.items by its index in the array
+    $scope.removeItem = function (id) {
+        $scope.items.splice(id, 1);
+    };
 });
